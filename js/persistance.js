f7d88f146cefe496754faae289a101fe8ebbb41e
Juan Jose Guarumo <Juanjosealonsoguarumo@gmail.com>
2026-03-29 16:25:33 -0500

    feat(js/persistance): add registrarBusqueda que guarda en historial y retorna lista actual

--- js/persistance.js
+++ js/persistance.js
@@ -1,9 +1,16 @@
 import Almacenamiento from './storage.js';
 
 const alternarFavorito = (serie) => {
-  const era = Almacenamiento.esFavorito(serie.id);
-  era
-    ? Almacenamiento.eliminarFavorito(serie.id)
-    : Almacenamiento.agregarFavorito(serie);
-  return { agregado: !era };
+    const era = Almacenamiento.esFavorito(serie.id);
+    era
+        ? Almacenamiento.eliminarFavorito(serie.id)
+        : Almacenamiento.agregarFavorito(serie);
+    return { agregado: !era };
+};
+
+/* -----------------*/
+
+const registrarBusqueda = (q) => {
+    Almacenamiento.agregarAlHistorial(q);
+    return Almacenamiento.obtenerHistorial();
 };
