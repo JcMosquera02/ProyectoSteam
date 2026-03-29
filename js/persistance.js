373ff4f8eac125e91283d11ee98a4cdc18952f89
Juan Jose Guarumo <Juanjosealonsoguarumo@gmail.com>
2026-03-29 16:24:38 -0500

    feat(js/persistance): add alternarFavorito con logica toggle y retorno de estado agregado

---
+++ js/persistance.js
@@ -0,0 +1,9 @@
+import Almacenamiento from './storage.js';
+
+const alternarFavorito = (serie) => {
+  const era = Almacenamiento.esFavorito(serie.id);
+  era
+    ? Almacenamiento.eliminarFavorito(serie.id)
+    : Almacenamiento.agregarFavorito(serie);
+  return { agregado: !era };
+};
